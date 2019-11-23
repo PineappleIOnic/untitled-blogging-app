18cd045db06ea1710d7523fa727eb3fedbaaf2d6
IOnicisere <ionicisere@gmail.com>
2019-11-23 16:35:53 +0000

    Update to babel 7
    + Fix bloody braces security notices.
    + Faster babel compiling due to specifically targetting node.
    + Just updating things is better

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+const presets = [
+    [
+      "@babel/env",
+      {
+        targets: {
+          node: "12",
+        },
+        useBuiltIns: "usage",
+        corejs: 3,
+      },
+    ],
+  ];
+  
+  module.exports = { presets };
