a88f5252364e6ffe213f59b2bab1ebf8fb7ce211
IOnicisere <ionicisere@gmail.com>
2019-11-27 23:12:54 +0000

    Fix 404's not sending statuscode correctly

--- src/routers/error_router.js
+++ src/routers/error_router.js
@@ -3,8 +3,7 @@ const router = express.Router();
 
 router.get("*", (req, res) => {
     // eslint-disable-next-line no-undef
-    res.status(404)
-    res.render('../src/views/error_view.ejs', {error_code:404, SITEKEY : process.env.CAPTCHA3_SITEKEY})
+    res.status(404).render('../src/views/error_view.ejs', {error_code:404, SITEKEY : process.env.CAPTCHA3_SITEKEY})
 })
 
 module.exports = router
